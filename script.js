1349599abcfbcef1dc62ee5764930ae75b621a01
Gracielle <graciellesampaio@outlook.com>
2023-09-03 15:50:05 -0300

    definindo variáveis

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-window.alert("Olá,mundo!")
+let nome =prompt("Qual o nome do seu pet") 
+let idade = Number;
+// para saber se ele está vivo ou morto
+let vida = true;
+
+
+let felicidade = document.getElementById('felicidade');
+let fome = document.getElementById('fome');
+
+let status = [nome, idade, vida]
+
+   
